bf5c13b46fcf4333a2d53153909df1ffabcf1898
Kashif <kashif.rasool@tuni.fi>
2024-10-30 16:36:35 +0200

    Add User data model and repository

---
+++ src/main/java/com/linkify/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.linkify.repository;
+
+import com.linkify.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+}
