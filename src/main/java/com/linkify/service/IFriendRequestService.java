cbf3733896d25edc0dbe9ebfc7fd566bd7471874
Kashif <kashif.rasool@tuni.fi>
2024-12-29 15:36:16 +0500

    Update FriendRequest service

--- src/main/java/com/linkify/service/IFriendRequestService.java
+++ src/main/java/com/linkify/service/IFriendRequestService.java
@@ -9,4 +9,5 @@ public interface IFriendRequestService {
     void sendFriendRequest(User sender, User receiver);
     void acceptFriendRequest(Long requestId);
     List<FriendRequest> getPendingRequests(User user);
+    List<User> getFriendsList(User user);
 }
