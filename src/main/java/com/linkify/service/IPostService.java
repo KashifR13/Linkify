02e3ac0d27ccd986506e8c77ca514f600e229aad
Kashif <kashif.rasool@tuni.fi>
2025-01-11 18:31:36 +0200

    Update Post service interface to add more methods

--- src/main/java/com/linkify/service/IPostService.java
+++ src/main/java/com/linkify/service/IPostService.java
@@ -6,6 +6,9 @@ import com.linkify.model.User;
 import java.util.List;
 
 public interface IPostService {
-    Post createPost(User author, String content, String mediaUrl);
+    Post createPost(User user, String content, String mediaUrl);
     List<Post> getNewsFeed(List<User> friends);
+    void likePost(Long postId, User user);
+    void commentOnPost(Long postId, User user, String comment);
+    List<Post> getPostsByUser(User user);
 }
